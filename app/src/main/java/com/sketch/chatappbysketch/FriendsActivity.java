1c56a292039c74c75e25f42e512000dd128a604b
Vipul Jatanna <jatannavips123@gmail.com>
2021-12-18 16:21:51 +0530

    first commit

---
+++ app/src/main/java/com/sketch/chatappbysketch/FriendsActivity.java
@@ -0,0 +1,33 @@
+package com.sketch.chatappbysketch;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class FriendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()== R.id.menu_item_profile){
+            startActivity(new Intent(FriendsActivity.this,Profile.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
